b23504e419ec28e7ea32b311d42582bf843e0ace
Sergej Kukharev <sergej.kucharev@gmail.com>
2017-03-14 04:35:26 +0200

    0.0.3b

--- test/index.js
+++ test/index.js
@@ -3,9 +3,11 @@
 'use strict';
 
 const assert = require('assert');
+const ZCookies = require('../lib/ZCookies');
 const ZCookie = require('../lib/ZCookie');
 
 (function () {
-  const c = new ZCookie('x', '67', {d: '?', m: 0, e: '2017-04-13 11:02:03 GMT', httpOnly: true, p: '?', sameSite: 'strict', secure: true});
+  const c = new ZCookie('x', '67Ґаліна', {d: 'i.ua', m: 0, e: '2017-04-13 11:02:03 GMT', p: '/', sameSite: true, secure: true});
   console.log(c);
+  console.log(String(c));
 })();
